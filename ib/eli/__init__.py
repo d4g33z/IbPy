1b9e5b7bf066605139770c50bc72c7f140422f5b
jeff <jeff@azeus>
2016-06-08 10:37:29 -0400

    Stripping out any direct ib java code and eliot refs.

--- ib/eli/__init__.py
+++ ib/eli/__init__.py
@@ -1,17 +1,5 @@
-import re
-
-from .EWrapperData import EWRAPPER_DATA
-
-
-RE = re.compile("\s*\w+\s+\w+\s+(\w+)\(\s*?(.*?)?,?\)")
-RE2 = re.compile("\s*?(.*)")
-
-ews = [[x for x in map(lambda x:x.replace('&','').split(','),ew)] for ew in \
-            [RE.match(x).groups() for x in \
-                [x for x in EWRAPPER_DATA.replace('\n','').split(';') if x != '']]]
-
-#strip out some leading spaces in parameters
-ews = [[ew[0],[re.match("\s*(.*)",x).groups()[0] for x in ew[1]]] for ew in ews]
+from .ewrapper_data import EWRAPPERS as ews
+from .eclient_data import ECLIENTS as ecs
 
 from copy import copy
 def flatten(x):
@@ -22,38 +10,20 @@ def flatten(x):
         if l: yield l.pop(0)
 
 #look at this to create type_models
-types_to_model = set([x for x in flatten([[x.split(' ')[:-1] for x in ew[1]] for ew in ews])])
+types_to_model = \
+    set([x for x in flatten([[x.split(' ')[:-1] for x in e[1]] for e in ews+ecs])])
 
-from ib.ext.CommissionReport import CommissionReport
-from ib.ext.Contract import Contract
-from ib.ext.ContractDetails import ContractDetails
-from ib.ext.Execution import Execution
-from ib.ext.UnderComp import UnderComp
-from ib.ext.Order import Order
-from ib.ext.OrderState import OrderState
-
-from ib.ext.ComboLeg import ComboLeg
-from ib.ext.OrderComboLeg import OrderComboLeg
+from ib.ext.CommissionReport    import CommissionReport
+from ib.ext.Contract            import Contract
+from ib.ext.ContractDetails     import ContractDetails
+from ib.ext.Execution           import Execution
+from ib.ext.UnderComp           import UnderComp
+from ib.ext.Order               import Order
+from ib.ext.OrderState          import OrderState
+from ib.ext.ComboLeg            import ComboLeg
+from ib.ext.OrderComboLeg       import OrderComboLeg
 from ib.ext.ScannerSubscription import ScannerSubscription
-
-
-class IBContract(object):
-    def __new__(cls,**kwargs):
-        obj = Contract()
-        [setattr(obj,'m_'+k,v) for k,v in kwargs.items()]
-        return obj
-
-    def get_property(self,name):
-        return getattr(self,'m_'+name)
-
-class IBComboLeg(object):
-    def __new__(cls,**kwargs):
-        obj = ComboLeg()
-        [setattr(obj,'m_'+k,v) for k,v in kwargs.items()]
-        return obj
-
-    def get_property(self,name):
-        return getattr(self,'m_'+name)
+from ib.ext.ExecutionFilter     import ExecutionFilter
 
 def IBObjectFactory(ibcls):
     class IBGeneric(object):
@@ -65,10 +35,10 @@ def IBObjectFactory(ibcls):
 
         def get_property(self,name):
             return getattr(self,'m_'+name)
-    #IBGeneric.__name__ = 'IB'+ibcls.__name__
     return IBGeneric
 
 type_models = {
+#from EWRAPPER_DATA
     'CommissionReport'  :CommissionReport,
     'Contract'          :Contract,
     'ContractDetails'   :ContractDetails,
@@ -89,16 +59,20 @@ type_models = {
     'ComboLeg'          :ComboLeg,
     'OrderComboLeg'     :OrderComboLeg,
     'ScannerSubscription':ScannerSubscription,
+
+#from ECLIENT_DATA
+    'ExecutionFilter'   :ExecutionFilter,
+    'std::string'       :str,
+    'tickerId'          :int
 }
 
-from eliot import MessageType,Field,add_destination
 def make_serializer(kls):
     ks = [x for x in filter(lambda x:'m_' in x,kls.__dict__.keys())]
     #summary attribute is a Contract type
     if kls is ContractDetails:
         return lambda x:{k[2:]:getattr(x,k) if k[2:] != 'summary' \
             else make_serializer(Contract)(getattr(x,k)) for k in ks}
-    elif kls is Contract: #why does it have an underComp attribute? What type is it?
+    elif kls is Contract:
         return lambda x:{k[2:]:getattr(x,k) if k[2:] != 'comboLegs' \
             else [make_serializer(ComboLeg)(y) for y in getattr(x,k)] for k in ks}
     else:
@@ -110,13 +84,15 @@ def make_deserializer(kls):
     if kls is ContractDetails:
         return lambda x:IBObjectFactory(kls)(**{k:v if k != 'summary' \
             else make_deserializer(Contract)(**v) for k,v in x.items()})
-    elif kls is Contract: #why does it have an underComp attribute? What type is it?
+    elif kls is Contract:
         return lambda x:IBObjectFactory(kls)(**{k:v if k != 'comboLegs' \
             else [make_deserializer(ComboLeg)(y) for y in v] for k,v in x.items()})
     else:
         return lambda x:IBObjectFactory(kls)(**x)
 
+from eliot import MessageType,Field,add_destination
 ELIOT_MESSAGE_TYPES = {}
+EC_ELIOT_MESSAGE_TYPES = {}
 
 for ew in ews:
     fields = []
@@ -143,8 +119,37 @@ for ew in ews:
     ELIOT_MESSAGE_TYPES[ew[0][0]] = \
             MessageType('ib_message',fields)
 
-from ib.opt import message
-ecs = [x for x in filter(lambda x:'_' not in x,message.__dict__.keys())]
+for ec in ecs:
+    fields = []
+    for type_info in ec[1]:
+        if type_info != '':
+            try:
+                _,ib_type,argument = \
+                [x for x in filter(lambda x:x is not '',type_info.split(' '))]
+            except ValueError:
+                ib_type,argument = \
+                    [x for x in filter(lambda x:x is not '',type_info.split(' '))]
+            if ib_type not in type_models.keys():
+                if ec[0][0] == 'reqFundamentalData':
+                    if argument != 'orderId': #orderId is ok
+                        ib_type = argument
+                        argument = argument[0].lower() + argument[1:]
+                else:
+                    print("{} is broken now too:-()".format(ec[0][0]))
+                    break
+
+            if ib_type in \
+                    ['TickType','IBString','OrderId','TickerId',
+                     'bool','double','faDataType','int','long',
+                     'std::string','tickerId']:
+                fields.append(Field.for_types(argument,[type_models[ib_type]],description=''))
+            elif ib_type not in ['TagValueListSPtr']:
+                fields.append(Field(argument,make_serializer(type_models[ib_type]),description=''))
+
+    EC_ELIOT_MESSAGE_TYPES[ec[0][0]] = \
+            MessageType('ib_message',fields)
+
+
 
 #__all__ = [IBContract,ELIOT_MESSAGE_TYPES,make_serializer]
 #example usage #notice the assignment!!!
